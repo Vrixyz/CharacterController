e0af49186e06ccac76e6970a6ff487b9730fdceb
Matthieu Bourgeon <matthieu.ossato-bourgeon@epitech.eu>
2014-03-20 17:40:20 +0900

    [Script] Character_Manager skeleton

--- Scripts/Character_Manager.cs
+++ Scripts/Character_Manager.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Character_Manager : MonoBehaviour {
 
-	public static CharacterControllerComponent Instance;
+	public static CharacterController Instance;
 
-	public float	deadZone;
+	public	float	deadZone;
 
 	void Awake()
 	{
@@ -16,9 +16,18 @@ public class Character_Manager : MonoBehaviour {
 	void Update()
 	{
 		// Camera_Manager.Instance.CheckCameraExists();
+
 		ControllerInput();
-		Character_Motor.Instance.ControlledUpdate();
+
 		Character_Motor.Instance.MoveVector = Vector3.zero;
+	
+		ActionInput();
+
+		Character_Motor.Instance.ApplyGravity();
+
+		Character_Motor.Instance.ProcessMotion();
+	
+		Character_Motor.Instance.ControlledUpdate();
 	}
 
 	void ControllerInput()
@@ -33,17 +42,19 @@ public class Character_Manager : MonoBehaviour {
 
 		if (vertical > deadZone || vertical < -deadzone)
 		{
-			Character_Motor.Instance.MoveVector += new Vector3(0, -vertical, 0);
+			Character_Motor.Instance.MoveVector += new Vector3(0, 0, -vertical);
 		}
 	}
 
 	void DelegateJump()
 	{
+		//Character_Animator.Instance.JumpAnimation();
 		Character_Motor.Instance.Jump();
 	}
 
 	void ActionInput()
 	{
-
+		if (Input.GetButton("Jump"))
+			DelegateJump();
 	}
 }
