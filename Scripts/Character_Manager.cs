169d8f7fa4566eb472a56553b8c2229c39e0387b
Matthieu Bourgeon <matthieu.ossato-bourgeon@epitech.eu>
2014-03-20 17:15:34 +0900

    [Script] Character_Manager

---
+++ Scripts/Character_Manager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Manager : MonoBehaviour {
+
+	public static CharacterControllerComponent Instance;
+
+	public float	deadZone;
+
+	void Awake()
+	{
+		Instance = this;
+		// Camera_Manager.Instance.CheckCameraExists();
+	}
+
+	void Update()
+	{
+		// Camera_Manager.Instance.CheckCameraExists();
+		ControllerInput();
+		Character_Motor.Instance.ControlledUpdate();
+		Character_Motor.Instance.MoveVector = Vector3.zero;
+	}
+
+	void ControllerInput()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		if (horizontal > deadZone || horizontal < -deadZone)
+		{
+			Character_Motor.Instance.MoveVector += new Vector3(horizontal, 0, 0);
+		}
+
+		if (vertical > deadZone || vertical < -deadzone)
+		{
+			Character_Motor.Instance.MoveVector += new Vector3(0, -vertical, 0);
+		}
+	}
+
+	void DelegateJump()
+	{
+		Character_Motor.Instance.Jump();
+	}
+
+	void ActionInput()
+	{
+
+	}
+}
