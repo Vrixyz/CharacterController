8c88b533f9ea1fefe9bd07ed3bef3a513471a8b3
leurqum <mathieu.leurquin@epitech.eu>
2014-06-03 11:28:21 +0900

    cvm

---
+++ Scripts/ClimbVolumeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbVolumeManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Animation_Manager am = other.gameObject.GetComponent<Animation_Manager>();
+
+        if (am != null)
+        {
+            am.SetClimbVolumeTransform(transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Animation_Manager am = other.gameObject.GetComponent<Animation_Manager>();
+
+        if (am != null)
+        {
+            am.ClearClimbVolumeTransform();
+        }
+    }
+}
